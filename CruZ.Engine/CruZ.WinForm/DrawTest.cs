56e857dfe384bc55d769d4690c2ae51c1495a550
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-01-07 16:51:20 +0700

    winform intergration complete

---
+++ CruZ.Engine/CruZ.WinForm/DrawTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CruZ.WinForm
+{
+    internal class DrawTest : MonoGame.Forms.NET.Controls.MonoGameControl
+    {
+        protected override void Initialize()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+        }
+
+        protected override void Draw()
+        {
+            Editor.spriteBatch.Begin();
+
+            Editor.spriteBatch.DrawString(Editor.Font, WelcomeMessage, new Vector2(
+                (500 / 2) - (Editor.Font.MeasureString(WelcomeMessage).X / 2),
+                (500 / 2) - (Editor.FontHeight / 2)),
+                Microsoft.Xna.Framework.Color.White);
+
+            Editor.spriteBatch.End();
+        }
+
+        string WelcomeMessage = "Hello MonoGame.Forms!";
+    }
+}
---
+++ CruZ.Engine/CruZ.WinForm/Form1.Designer.cs
@@ -0,0 +1,43 @@
+﻿
+namespace CruZ.WinForm
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+            this.Controls.Add(_drawTest);
+
+            _drawTest.Size = new Size(350, 399);
+            _drawTest.Location = new Point(0, 0);
+
+        }
+
+        #endregion
+
+        private DrawTest _drawTest = new DrawTest();
+    }
+}
