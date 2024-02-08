ed3dace1bcd52afe6df590eeee9ac701cc68923b
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-02-08 16:14:17 +0700

    grid move

--- CruZ.Games/AnimalGang/AnimalGang.Shared/src/MainCharacter.cs
+++ CruZ.Games/AnimalGang/AnimalGang.Shared/src/MainCharacter.cs
@@ -3,6 +3,7 @@ using CruZ.Systems;
 using CruZ.Utility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
 using System;
 
 namespace CruZ.Games.AnimalGang
@@ -25,6 +26,7 @@ namespace CruZ.Games.AnimalGang
 
         protected override void OnUpdate(GameTime gameTime)
         {
+            #region AttackLogics
             if (Input.Info.Keyboard.IsKeyDown(Keys.Space))
             {
                 _attackTimer = 0;
@@ -35,15 +37,34 @@ namespace CruZ.Games.AnimalGang
             {
                 _attackTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 return;
+            } 
+            #endregion
+
+            if(_remainDis >= 0)
+            {
+                _remainDis -= _speed * gameTime.GetElapsedSeconds();
+
+                AttachedEntity.Transform.Position +=
+                    _moveDir * _speed * gameTime.GetElapsedSeconds();
+            }
+            else
+            {
+                SnapPosition();
+
+                _moveDir = GetMovingInput();
+
+                if(_moveDir.SqrMagnitude() > 0.1)
+                {
+                    _remainDis = 1;
+                }
             }
 
-            _moveDir = GetMovingInput();
-
-            if(_moveDir.SqrMagnitude() > 0.1f)
+            #region AnimationLogics
+            if (_moveDir.SqrMagnitude() > 0.1f)
             {
                 _animation.SelectPlayer("player-normal").Play("walk");
 
-                if(MathF.Abs(_moveDir.X) > 0.1)
+                if (MathF.Abs(_moveDir.X) > 0.1)
                 {
                     _sprite.Flip = _moveDir.X < 0;
                 }
@@ -51,10 +72,19 @@ namespace CruZ.Games.AnimalGang
             else
             {
                 _animation.SelectPlayer("player-sword-idle").Play("idle");
-            }
+            } 
+            #endregion
+        }
 
-            AttachedEntity.Transform.Position += 
-                _moveDir * _speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        private void SnapPosition()
+        {
+            var px = AttachedEntity.Transform.Position.X;
+            var py = AttachedEntity.Transform.Position.Y;
+
+            px = MathF.Ceiling(px) - 0.5f;
+            py = MathF.Ceiling(py) - 0.5f;
+
+            AttachedEntity.Transform.Position = new(px, py);
         }
 
         private Vector3 GetMovingInput()
@@ -63,29 +93,23 @@ namespace CruZ.Games.AnimalGang
 
             if (Input.Info.Keyboard.IsKeyDown(Keys.A))
             {
-                moveDir += new Vector3(-1, 0);
+                moveDir = new Vector3(-1, 0);
             }
             if (Input.Info.Keyboard.IsKeyDown(Keys.D))
             {
-                moveDir += new Vector3(1, 0);
+                moveDir = new Vector3(1, 0);
             }
             if (Input.Info.Keyboard.IsKeyDown(Keys.S))
             {
-                moveDir += new Vector3(0, 1);
+                moveDir = new Vector3(0, 1);
             }
             if (Input.Info.Keyboard.IsKeyDown(Keys.W))
             {
-                moveDir += new Vector3(0, -1);
+                moveDir = new Vector3(0, -1);
             }
 
             return moveDir;
         }
-
-        //bool Arrived()
-        //{
-        //    var delt = GetSnapPos() - AttachedEntity.Transform.Position;
-        //    return delt.SqrMagnitude() > 0.1;
-        //}
 
         private Vector3 GetSnapPos()
         {
@@ -100,7 +124,8 @@ namespace CruZ.Games.AnimalGang
 
         float _speed = 6;
         Vector3 _moveDir;
-        bool _isArrvied = false;
+        float _remainDis = 0;
+        bool _moving = false;
 
         float _attackDuration = 0.2f;
         float _attackTimer = 9999999f;
