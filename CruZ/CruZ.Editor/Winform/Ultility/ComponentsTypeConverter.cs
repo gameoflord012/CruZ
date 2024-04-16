95b72e4970544a4b12e8737b14e042aa7c6e9e54
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-04-16 22:11:57 +0700

    Add todo

--- CruZ/CruZ.Editor/Winform/Ultility/ComponentsTypeConverter.cs
+++ CruZ/CruZ.Editor/Winform/Ultility/ComponentsTypeConverter.cs
@@ -6,6 +6,7 @@ using System.Linq;
 
 namespace CruZ.Editor.Winform.Ultility
 {
+    // TODO: subtitude this to use TransformEntityTypeConverter directly instead of using EntityWrapper
     public class ComponentsWrapperTypeConverter : ExpandableObjectConverter
     {
         public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext? context, object value, Attribute[]? attributes)
